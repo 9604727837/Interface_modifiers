ecd60c94620d4f08fee61be3565d2fb5c91a771e
Rupesh Singh <123446052+9604727837@users.noreply.github.com>
2023-10-18 17:30:36 +0530

    Add files via upload

---
+++ Interface_1/src/Interface_declaration/Interface_declaration.java
@@ -0,0 +1,46 @@
+package Interface_declaration;
+//
+//interface interf{
+//	 public void m1();                 //interface method always public and abstract
+//	 abstract public void m2();
+//
+//}
+//abstract class serviceprovider implements interf{
+//	public void m1() {          
+//		                          // abstract class with public method implementation
+//	}
+//	
+//public class Interface_declaration extends serviceprovider {
+//	public void m2() {
+//		int y=20;                // extended abstract class 
+//		
+//	}
+
+//}
+//}
+
+//
+//interface intere{
+//	void m10();
+//	public void m11();                  types of way we can declare method in interface 
+//	abstract void m12();               as every interface method is always public and abstract 
+//	public abstract void m13();
+//	
+//}
+
+
+//
+//interface interg{
+//	int x;              for interface variable compulsory we should perform intilization at the time of declaration;
+//}
+
+
+//interface interh{
+//     int m=10;
+//}
+//class test implements interh{
+//	public static void main(String[] args) {     inside implementation class we can access interface variable but we cant modify values
+//		int x = 777;
+//		System.out.println(x);
+//	}
+//}
---
+++ Interface_1/src/Interface_declaration/Interface_naming_conflicts.java
@@ -0,0 +1,51 @@
+package Interface_declaration;
+//interface left{
+//	public void m1();
+//}
+//interface right{
+//	public void m1();
+//}
+//public class Interface_naming_conflicts implements left,right {
+//	public void m1() {                     
+//		
+//	}
+//	public static void main(String[] args) {
+//
+//	}
+//
+//}
+//interface left{
+//public void m1();
+//}
+//interface right{
+//public void m1(int i);
+//}
+//public class Interface_naming_conflicts implements left,right {
+//public void m1() {                     
+//	                                  if the method name is same but argument is differnt the  we hav to provide implementation for each method (i.e overloading) 
+//}
+//public void m1(int i) {                  
+//	
+//}
+//public static void main(String[] args) {
+//
+//}
+//
+//}
+//interface left{
+//public void m1();
+//}
+//interface right{
+//public int m1();
+//}
+//public class Interface_naming_conflicts implements left,right {
+//public void m1() {                     
+//	                                   overridden is not supported
+//}
+//class subset extends Interface_naming_conflicts{
+//	public int m1() {
+//		return10;
+//	}
+//}
+//
+//}
